e693cea1974b1767d81b6edcc421b49b2714d485
Marinosbitter <marijn_bitter@live.nl>
2015-10-13 13:55:27 +0200

    Added database package and classes

---
+++ DeSleutelaar/app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/database/DatabaseHelper.java
@@ -0,0 +1,62 @@
+package nl.hsleiden.imtpmd.desleutelaar.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+
+/**
+ * Created by Marijn on 13/10/2015.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static SQLiteDatabase mSQLDB;
+    private static DatabaseHelper mInstance;
+
+    public static final String dbName = "barpmeter.db";
+    public static final int dbVersion = 1;
+
+    public DatabaseHelper(Context ctx) {
+        super(ctx, dbName, null, dbVersion);
+    }
+
+    public static synchronized DatabaseHelper getHelper(Context ctx) {
+        if (mInstance == null) {
+            mInstance = new DatabaseHelper(ctx);
+            mSQLDB = mInstance.getWritableDatabase();
+        }
+        return mInstance;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + DatabaseInfo.CourseTables.COURSE + " (" +
+                        BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        DatabaseInfo.CourseColumn.NAME + " TEXT," +
+                        DatabaseInfo.CourseColumn.ECTS + " TEXT," +
+                        DatabaseInfo.CourseColumn.GRADE + " REAL);"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseInfo.CourseTables.COURSE);
+        onCreate(db);
+    }
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public void insert(String table, String nullColumnHack, ContentValues values) {
+        mSQLDB.insert(table, nullColumnHack, values);
+    }
+
+
+    public Cursor query(String table, String[] columns, String selection, String[] selectArgs, String groupBy, String having, String orderBy) {
+        return mSQLDB.query(table, columns, selection, selectArgs, groupBy, having, orderBy);
+    }
+}
---
+++ DeSleutelaar/app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/database/DatabaseInfo.java
@@ -0,0 +1,16 @@
+package nl.hsleiden.imtpmd.desleutelaar.database;
+
+/**
+ * Created by Marijn on 13/10/2015.
+ */
+public class DatabaseInfo {
+    public class CourseTables {
+        public static final String COURSE = "course";
+    }
+
+    public class CourseColumn {
+        public static final String NAME = "name";
+        public static final String ECTS = "ects";
+        public static final String GRADE = "grade";
+    }
+}
